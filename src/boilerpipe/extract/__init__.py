81e8423f6eb7d8a41d339aff513507f8a4076430
Shosuke Kitano <shosuke@guruguru.com>
2013-11-28 12:21:36 +0900

    Separate instance creation and source settings.

--- src/boilerpipe/extract/__init__.py
+++ src/boilerpipe/extract/__init__.py
@@ -1,7 +1,8 @@
+
 import jpype
-import urllib2
+import requests
 import socket
-import charade
+import chardet
 import threading
 
 socket.setdefaulttimeout(15)
@@ -28,24 +29,9 @@ class Extractor(object):
     extractor = None
     source    = None
     data      = None
-    headers   = {'User-Agent': 'Mozilla/5.0'}
+    headers   = {'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.162 Safari/535.19'}
     
-    def __init__(self, extractor='DefaultExtractor', **kwargs):
-        if kwargs.get('url'):
-            request     = urllib2.Request(kwargs['url'], headers=self.headers)
-            connection  = urllib2.urlopen(request)
-            self.data   = connection.read()
-            encoding    = connection.headers['content-type'].lower().split('charset=')[-1]
-            if encoding.lower() == 'text/html':
-                encoding = charade.detect(self.data)['encoding']
-            self.data = unicode(self.data, encoding)
-        elif kwargs.get('html'):
-            self.data = kwargs['html']
-            if not isinstance(self.data, unicode):
-                self.data = unicode(self.data, charade.detect(self.data)['encoding'])
-        else:
-            raise Exception('No text or url provided')
-
+    def __init__(self, extractor):
         try:
             # make it thread-safe
             if threading.activeCount() > 1:
@@ -54,14 +40,34 @@ class Extractor(object):
             lock.acquire()
             
             self.extractor = jpype.JClass(
-                "de.l3s.boilerpipe.extractors."+extractor).INSTANCE
+                "de.l3s.boilerpipe.extractors." + extractor).INSTANCE
         finally:
             lock.release()
-    
+
+
+    def setUrl(self, url):
+        response = requests.get(url, headers=self.headers)
+        content_type = response.headers.get('content-type')
+        if content_type.lower().startswith('text/html'):
+            content_encoding = chardet.detect(response.content)['encoding']
+            if response.encoding.lower() != content_encoding.lower():
+                self.data = response.content.decode(content_encoding)
+            else:
+                self.data = response.text
+        else:
+            raise Exception('Invalid content type')
+
+
+    def setHtml(self, html):
+        self.data = html
+
+    def process(self):
+        if self.data is None:
+            raise Exception('No text or url provided')
         reader = StringReader(self.data)
         self.source = BoilerpipeSAXInput(InputSource(reader)).getTextDocument()
         self.extractor.process(self.source)
-    
+
     def getText(self):
         return self.source.getContent()
     
